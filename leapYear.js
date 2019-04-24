9c32803cf80150cd57a50dafc9580085d37ea49c
nuwayyir1993 <nuwayyir.almohammadi@outlook.com>
2019-04-24 08:40:53 +0000

    I have done the Bouns now

--- leapYear.js
+++ leapYear.js
@@ -1,5 +1,7 @@
 const leapyear=function(year){
-    if (year%4==0 && year%100 !=0)
+    if (year<0 || typeof(year)!="number")
+    return ("please enter a valid year number ");
+    else if (year%4==0 && year%100 !=0)
       return ("leapyear");
      else if (year%400==0)
       return ("leap year");
